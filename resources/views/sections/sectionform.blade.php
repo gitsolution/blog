78c7e4553f562748ef19dded94cd4d2c1e6bdbb9
Herrera Lopez Jose Raul <raul_herreralsc@hotmail.com>
2016-02-08 15:15:20 -0600

    Formulario de secciones

--- resources/views/sections/sectionform.blade.php
+++ resources/views/sections/sectionform.blade.php
@@ -3,72 +3,74 @@
        <!---contact-->
 
 <?php  
-if(isset($Section)) {
-$botonTitulo='Editar'; // para cambiar de nombre al submit si es editar o guardar
- $message='Edit';
- $id_type=$Section->id_type;
- $title=$Section->title;
- $resumen=$Section->resumen;
- $content=$Section->content;
- $main_picture=$Section->main_picture;
+if(isset($Section)) 
+{
+    $botonTitulo='Editar'; // para cambiar de nombre al submit si es editar o guardar
+    $message='Edit';
+    $id_type=$Section->id_type;
+    $title=$Section->title;
+    $resumen=$Section->resumen;
+    $content=$Section->content;
+    $main_picture=$Section->main_picture;
   if($Section->private=="0")
- {
-  $ChekPrivado = "";//;  
- }
- else{
-  $ChekPrivado = "checked";//;  
- } 
- $publish_date = date_create($Section->publish_date);
- if($Section->publish=="0")
- {
-  $ChekPublicar = "";//;  
- }
- else{
-  $ChekPublicar = "checked";//;  
- } 
- $uri=$Section->uri;
- $hits=$Section->hits;
- $order_by= $Section->order_by;
+      {
+        $ChekPrivado = "";
+      }
+  else{
+        $ChekPrivado = "checked"; 
+      } 
+    $publish_date = date_create($Section->publish_date);
+  if($Section->publish=="0")
+      {
+        $ChekPublicar = "";  
+      }
+  else{
+        $ChekPublicar = "checked"; 
+      } 
+    $uri=$Section->uri;
+    $hits=$Section->hits;
+    $order_by= $Section->order_by;
 
 
-}else{ 
-$botonTitulo='Crear';
-  $message='New'; 
- $Section=Null;
- $title=$Section;
- $description=$Section;
- $publish = $Section;
- $publish_date= $Section;
- $order_by= $Section;
- $id_type=$Section;
 }
+
+else{ 
+    $botonTitulo=' Crear';
+    $message='New'; 
+    $Section=Null;
+    $title=$Section;
+    $description=$Section;
+    $ChekPrivado=$Section;
+    $ChekPublicar = $Section;
+    $publish_date= $Section;
+    $order_by= $Section;
+    $id_type=$Section;
+  }
  ?>
 
 @if($message=='Edit')
  {!!Form::model($Section,['route'=>['admin.sections.update',$Section->id],'method'=>'PUT'])!!} 
 @else
- {!!Form::open(['route'=>'admin.sections.store','method'=>'POST'])!!}
+ {!!Form::open(['route'=>'admin.sections.store','method'=>'POST', 'file'=>true])!!}
 @endif
 
 <div class="container-fluid">
- 
   <div class="container-fluid">
        <div class="row">
             <div class="form-group">
                   <div class="col-md-12"><h3 class="head">SECCION</h3>
-                       <p>PAGINA PARA LA SECCION</p></div>
+                      <p>PAGINA PARA LA SECCION</p>
+                  </div>
                 <div class="col-md-3">
-                     
                   {!!Form::label('tipo','Tipo:')!!}
-                 
-                  {!!Form::select('id_type', [1,2,3], null, ['class'=>'form-control input-md']) !!}
-              <br>
+                  {!!Form::select('id_type', \App\cms_type::lists('title','id'),null,['class'=>'form-control select2'] )!!}
+                <br>
                 </div>
                 <div class="col-md-5">
                   <div class="publiChec">
                       {!!Form::label('privado','Publicar')!!}
                           <div class="material-switch pull-right">
-                            <input id="someSwitchOptionInfo" name="ChekPublicar" checked="true" type="checkbox"/>
+                            <input id="someSwitchOptionInfo" name="ChekPublicar" <?php echo $ChekPublicar ?> type="checkbox"/>
                             <label for="someSwitchOptionInfo" class="label-info"></label>
                         </div>     
                   </div>
@@ -78,7 +80,7 @@ $botonTitulo='Crear';
                   <div class="priChec">
                       {!!Form::label('privado','Privado')!!}
                       <div class="material-switch pull-right">
-                          <input id="someSwitchOptionSuccess" name="ChekPrivado" checked="true" type="checkbox"/>
+                          <input id="someSwitchOptionSuccess" name="ChekPrivado" <?php echo $ChekPrivado ?>  type="checkbox"/>
                           <label for="someSwitchOptionSuccess" class="label-success"></label>
                       </div>           
                   </div>
@@ -111,9 +113,9 @@ $botonTitulo='Crear';
           </div>
          <div class="form-group">
                      {!!Form::label('Imagen Principal')!!}
-                     {!!Form::file('main_picture',['class'=>'btn btn-primary'])!!}
+                     {!!Form::file('main_picture')!!}
           </div>
-          {!!Form::submit($botonTitulo,['class'=>'btn btn-danger'])!!}
+          {!!Form::submit( $botonTitulo,['class'=>'btn btn-danger'])!!}
 
         {!!Form::close()!!} 
           

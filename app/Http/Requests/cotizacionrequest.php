ed5f858bd906a20fc3510b8b1dfbbdf6616f50ce
iver fabian gonzalez vidal <ft.pumas@hotmail.com>
2016-02-26 10:25:05 -0600

    se agrega validacion de captcha validacion de formulario contacto

--- app/Http/Requests/cotizacionrequest.php
+++ app/Http/Requests/cotizacionrequest.php
@@ -24,6 +24,7 @@ class cotizacionrequest extends Request
     public function rules()
     {
         return [           
+            'name'=>'required|alpha|min:2|max:70|max:255',
             'g-recaptcha-response' => 'required|recaptcha',
         ];
     }
@@ -31,6 +32,11 @@ class cotizacionrequest extends Request
      public function messages()
     {
         return [
+            'name.required'=>'Por favor complete su nombre',
+            'name.alpha'=>'El nombre solo debe contener letras',
+            'name.min'=>'El nombre debe tener al menos 2 caracteres',
+            'name.max'=>'El nombre debe tener máximo 70 caracteres',
+
             'g-recaptcha-response.required'=>'El campo captcha es requerido',
             'g-recaptcha-response.recaptcha'=>'Captcha incorrecto',
 
